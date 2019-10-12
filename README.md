77d76b69c6b1f92d44bc2b6d69126023a8921070
vencax <vencax@centrum.cz>
2019-10-12 10:44:04 +0200

    drivers used

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ picocom /dev/ttyACM0
 Nebo primo:
 ```
 wget https://raw.githubusercontent.com/micropython/micropython/master/tools/pyboard.py
-python pyboard.py 
+python pyboard.py
 python ~/pyboard.py /media/vencax/4621-0000/boot.py
 ```
+
+
+https://gitlab.com/georgedorn/tinysnakes/blob/master/client/htu21d.py
+
+
+https://forum.micropython.org/viewtopic.php?t=647
+https://github.com/pkucmus/micropython-pms7003/
--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 # import machine
-from pyb import Pin, ADC, Timer, UART
-from machine import I2C
+from pyb import Pin, ADC, Timer, UART, LED
+from pyb import I2C
+from client_htu21d import HTU21D
+from pms7003_passive import PassivePMS7003, IncorrectData
 
 import micropython
 micropython.alloc_emergency_exception_buf(100)
 
+workLed = LED(2)    # 1=red, 2=green, 3=yellow, 4=blue
+noiseLed = LED(1)
 pinNoiseThreshold = Pin('X2', Pin.IN, Pin.PULL_UP)
 noiseLevelADC = ADC(Pin('X11'))
 # siranLevelADC = ADC(Pin('X12'))
-# tempI2C = I2C('X', freq=400000)
-# PMxUART = UART(4, 9600) # X1 - tx, X2 - rx
-#
-# def sensePMx():
-#   dustLevel = PMxUART.read(5)
-#
-# def senseTemp():
-#   i2c.scan()                          # returns list of slave addresses
-#   i2c.readfrom(0x42, 5)
-
-def cback(t):
-  global overNoisedCount
-  print(overNoisedCount)
+tempDriver = HTU21D(I2C(2, I2C.MASTER))
+dustDriver = PassivePMS7003(UART(4))    # X1 - tx, X2 - r
 
 # ---------------------------
 
-freq = 1 / 5  # once per 100s
-tim = Timer(1, freq=freq)
-tim.callback(cback)
+temperature = 0
+humidity = 0
 
+def do_work(_):
+    workLed.on()
+    temperature = tempDriver.readTemperatureData()
+    humidity = tempDriver.readHumidityData()
+    print(temperature)
+    print(humidity)
+    dustDriver.wakeup()
+    try:
+        rr = dustDriver.read()
+        print(str(rr))
+    except IncorrectData:
+        print('bad data')
+    finally:
+        dustDriver.sleep()
+    workLed.off()
+
+freq = 1 / 8  # once per 100s
+tim = Timer(1, freq=freq)
+tim.callback(lambda _: micropython.schedule(do_work, 0))
 
 # ---------  SOUND  ------------------------------------------------------------
-
 avgSound = 0
 n = 1
 overNoisedCount = 0
@@ -38,6 +48,7 @@ noiseLevel = 0
 
 def senseNoise(t):
   global n, noiseLevel, overNoisedCount
+  noiseLed.toggle()
   val = pinNoiseThreshold.value()
   if val == 0:      # 0 = overnoise
     overNoisedCount += 1
